fb57e73cff15d36f10d226ce621050375a9dbe0a
Ярослав <spartacus215@yandex.ru>
2023-05-01 20:46:59 +0300

    Главная страница. Сделал десктоп и планшет

---
+++ source/js/slider.js
@@ -0,0 +1,9 @@
+/* eslint-disable camelcase */
+/* eslint-disable no-undef */
+$(() =>{
+  $('#cats-container').twentytwenty({
+    no_overlay: true,
+    move_with_handle_only: true,
+    click_to_move: false
+  });
+});
